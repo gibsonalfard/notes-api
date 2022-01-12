09b363b69ebec73e39cd06b495e3572b74db3009
Ilham Gibran <ilgibrz@gmail.com>
2022-01-12 23:52:55 +0700

    Remove unnecessary error handler

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -1,5 +1,3 @@
-const { ClientError } = require('../../exceptions');
-
 class NotesHandler {
   constructor(service, validator) {
     this.service = service;
@@ -82,35 +80,16 @@ class NotesHandler {
    * @returns {object}
    */
   async putNoteByIdHandler(request, h) {
-    try {
-      const { id } = this.validator.validateNoteParams(request.params);
-      const { title, tags, body } = this.validator.validateNotePayload(request.payload);
-      await this.service.updateNoteById(id, { title, tags, body });
+    const { id } = this.validator.validateNoteParams(request.params);
+    const { title, tags, body } = this.validator.validateNotePayload(request.payload);
+    await this.service.updateNoteById(id, { title, tags, body });
 
-      const response = h.response({
-        status: 'success',
-        message: 'Catatan berhasil diperbarui',
-      });
-      response.code(200);
-      return response;
-    } catch (error) {
-      if (error instanceof ClientError) {
-        const response = h.response({
-          status: 'fail',
-          message: error.message,
-        });
-        response.code(error.statusCode);
-        return response;
-      }
-
-      const response = h.response({
-        status: 'error',
-        message: 'INTERNAL_SERVER_ERROR',
-      });
-      response.code(500);
-      console.error(error);
-      return response;
-    }
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil diperbarui',
+    });
+    response.code(200);
+    return response;
   }
 
   /**
@@ -120,33 +99,14 @@ class NotesHandler {
    * @returns {object}
    */
   async deleteNoteByIdHandler(request, h) {
-    try {
-      const { id } = this.validator.validateNoteParams(request.params);
-      await this.service.deleteNoteById(id);
+    const { id } = this.validator.validateNoteParams(request.params);
+    await this.service.deleteNoteById(id);
 
-      const response = h.response({
-        status: 'success',
-        message: 'Catatan berhasil dihapus',
-      });
-      return response;
-    } catch (error) {
-      if (error instanceof ClientError) {
-        const response = h.response({
-          status: 'fail',
-          message: error.message,
-        });
-        response.code(error.statusCode);
-        return response;
-      }
-
-      const response = h.response({
-        status: 'error',
-        message: 'INTERNAL_SERVER_ERROR',
-      });
-      response.code(500);
-      console.error(error);
-      return response;
-    }
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil dihapus',
+    });
+    return response;
   }
 }
 
